d9f4dfa335ce5cd0103a7a09368ca81c1d686da3
sirojiddin <sirojiddinoyosboyev.com>
2025-08-22 11:23:26 +0500

    fayllar qo'shildi

--- task7.js
+++ task7.js
@@ -3,9 +3,9 @@ function conversition(son){
         return "Xatolik"
     }
     let b=+prompt("Dollar kursi qancha")
-    a=parseInt(son)*b
-    return a.toFixed(2)
+    a=parseInt(son)/b
+    return `${a.toFixed(2)}USD`
 
 }
-let son=+prompt("Pul miqdori qancha")
+let son=+prompt("Pul miqdori qancha so'm")
 alert(conversition(son))
